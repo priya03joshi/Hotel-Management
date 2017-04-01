80ee834c166cf48fcd7174d30a662ba4492b2e8a
priya03joshi <priyabdjoshi@gmail.com>
2017-04-01 23:33:02 +0530

    Add files via upload

---
+++ s_hotel_website/dining.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="styles/reset.css">
+	<link rel="stylesheet" href="styles/dining.css">
+	<title>Dining</title>
+</head>
+
+<body>
+    <img class="dining1" src="images/dining_main4.jpg"/>
+	<nav>
+	<ul class="dining">
+		<li><a href="dining"><b>Dining:</b></a></li>
+		<li><a href="dining_room"><b>The Dining Room</b></a></li>
+		<li><a href="private_dining"><b>Private Dining</b></a></li>
+		<li><a href="in_room_dining"><b>In Room Dining</b></a></li>
+	</ul>
+	<br>
+	<h2 class="logo"><img src="images/florida_logo.png"></h2>
+	<h3 class="enjoy"><img src="images/experience.png"></h3>
+	<br>
+	<h4 class="para"><img src="images/para_dining.png"/></h4>
+	
+	<ul class="photo">
+	<li>
+	<a href="dining_room"><img src="images/d1.png"/></a>
+	</li>
+	<li>
+	<a href="private_dining"><img src="images/d2.png"/></a>
+	</li>
+	<li>
+	<a href="in_room_dining"><img src="images/d3.png"/></a>
+	</li>
+	</ul>
+</body>
+</html>
---
+++ s_hotel_website/gallery.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="styles/reset.css">
+	<link rel="stylesheet" href="styles/gallery.css">
+	<title>Gallery</title>
+</head>
+
+<body>
+	<b><h1>Our Gallery</h1></b>
+	<img class="photo" src="images/p1.jpg"/>
+	<div class="parallax"></div>
+	<div class="parallax1"></div>
+	<div class="parallax2"></div>
+	<div class="parallax3"></div>
+	<div class="parallax4"></div>
+	<div class="parallax5"></div>
+	<div class="parallax6"></div>
+	<div class="parallax7"></div>
+	<div class="parallax8"></div>
+	<div class="parallax9"></div>
+	<div class="parallax10"></div>
+</body>
+</html>
